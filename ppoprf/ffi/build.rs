a9ac534682bb1ac12f551357a450843315e42b17
Ralph Giles <rgiles@brave.com>
2022-02-28 09:02:36 -0800

    ffi: Fix build.rs formatting lint.
    Ran `cargo fmt` to correct github ci failure.

--- ppoprf/ffi/build.rs
+++ ppoprf/ffi/build.rs
@@ -6,6 +6,6 @@ fn main() {
     let crate_dir = env::var("CARGO_MANIFEST_DIR").unwrap();
 
     cbindgen::generate(&crate_dir)
-      .expect("Unable to generate C bindings")
-      .write_to_file("include/ppoprf.h");
+        .expect("Unable to generate C bindings")
+        .write_to_file("include/ppoprf.h");
 }
