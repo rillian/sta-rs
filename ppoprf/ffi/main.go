3788d28f3d0236bef6fe915f9c0bc64f78feb9bc
Philipp Winter <phw@brave.com>
2022-03-09 09:30:16 -0800

    Make code more robust and add tests.

--- ppoprf/ffi/main.go
+++ ppoprf/ffi/main.go
@@ -8,12 +8,15 @@ package main
 import "C"
 
 import (
+	"encoding/hex"
 	"errors"
 	"fmt"
 	"log"
 	"net/http"
 	"os"
 	"runtime"
+	"sync"
+	"time"
 	"unsafe"
 
 	// This module must be imported first because of its side effects of
@@ -26,7 +29,13 @@ import (
 
 var (
 	elog = log.New(os.Stderr, "randsrv: ", log.Ldate|log.Ltime|log.LUTC|log.Lshortfile)
+
+	errNoECPoint     = "no 'ec_point' parameter given"
+	errDecodeECPoint = "failed to decode EC point"
+	errParseECPoint  = "failed to parse EC point"
 )
+
+type epoch uint8
 
 // Embed an zero-length struct to mark our wrapped structs `noCopy`
 //
@@ -47,8 +56,9 @@ func (*noCopy) Unlock() {}
 
 // Server represents a PPOPRF randomness server instance.
 type Server struct {
-	raw    *C.RandomnessServer
-	noCopy noCopy
+	sync.Mutex // TODO: Do we really need a mutex?
+	raw        *C.RandomnessServer
+	noCopy     noCopy
 }
 
 func serverFinalizer(server *Server) {
@@ -56,48 +66,73 @@ func serverFinalizer(server *Server) {
 	server.raw = nil
 }
 
-// createServer creates a new PPOPRF randomness server instance.
+// NewServer returns a new PPOPRF randomness server instance.
 //
 // FIXME Pass in a list of 8-bit tags defining epochs.
 // The instance will generate its own secret key.
-func createServer() (*Server, error) {
+func NewServer() (*Server, error) {
 	// FIXME should we runtime.LockOSThread() here?
 	raw := C.randomness_server_create()
 	if raw == nil {
-		return nil, errors.New("Failed to create randomness server")
+		return nil, errors.New("failed to create randomness server")
 	}
 	server := &Server{raw: raw}
 	runtime.SetFinalizer(server, serverFinalizer)
 	return server, nil
 }
 
-func getRandomnessHandler() http.HandlerFunc {
-	srv, err := createServer()
-	if err != nil {
-		elog.Fatalf("Failed to create randomness server: %s", err)
-	}
-	var c ristretto.Point
-	var md uint8 = 0
-	var verifiable bool
-	var output [32]byte
+// getEpoch returns the ISO week number of the given timestamp.
+func getEpoch(t time.Time) epoch {
+	_, week := t.ISOWeek()
+	return epoch(week)
+}
 
+// getRandomnessHandler returns an http.HandlerFunc so that we can pass our
+// server object into
+func getRandomnessHandler(srv *Server) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
-		c.Rand()
-		input, err := c.MarshalBinary()
-		if err != nil {
-			elog.Fatalf("Failed to marshal Ristretto point: %s", err)
+		encodedPoint := r.URL.Query().Get("ec_point")
+		if encodedPoint == "" {
+			http.Error(w, errNoECPoint, http.StatusBadRequest)
+			return
 		}
 
+		// Remove layer of hexadecimal encoding from marshalled EC point.
+		marshalledPoint, err := hex.DecodeString(encodedPoint)
+		if err != nil {
+			http.Error(w, errDecodeECPoint, http.StatusBadRequest)
+			return
+		}
+
+		// Check if we can parse the given EC point.  If it's un-parseable, we
+		// don't need to bother passing the point over our FFI.
+		var p ristretto.Point
+		if err := p.UnmarshalBinary(marshalledPoint); err != nil {
+			http.Error(w, errParseECPoint, http.StatusBadRequest)
+			return
+		}
+
+		var input []byte = []byte(marshalledPoint)
+		var verifiable bool = false
+		var output [32]byte // Ristretto points are 32 bytes long.
+		var md uint8 = 0
 		C.randomness_server_eval(srv.raw,
 			(*C.uint8_t)(unsafe.Pointer(&input[0])),
 			(C.ulong)(md),
 			(C.bool)(verifiable),
 			(*C.uint8_t)(unsafe.Pointer(&output[0])))
+
 		fmt.Fprintf(w, "%x", output)
 	}
 }
 
 func main() {
+	srv, err := NewServer()
+	if err != nil {
+		elog.Fatalf("Failed to create randomness server: %s", err)
+	}
+	elog.Println("Started randomness server.")
+
 	enclave := nitro.NewEnclave(
 		&nitro.Config{
 			SOCKSProxy: "socks5://127.0.0.1:1080",
@@ -107,7 +142,7 @@ func main() {
 			UseACME:    false,
 		},
 	)
-	enclave.AddRoute(http.MethodGet, "/randomness", getRandomnessHandler())
+	enclave.AddRoute(http.MethodGet, "/randomness", getRandomnessHandler(srv))
 	if err := enclave.Start(); err != nil {
 		elog.Fatalf("Enclave terminated: %v", err)
 	}
